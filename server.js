441e17076002ff79e36823a9cbffb17eecee4fc6
Gil Eckstein <gileck@wix.com>
2018-02-11 14:50:39 +0200

    commit

--- server.js
+++ server.js
@@ -1,39 +1,44 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const app = express();
+const key = fs.readFileSync('./server.key');
+const cert = fs.readFileSync('./server.crt' );
+
+const options = {
+    key: key,
+    cert: cert
+};
+
 app.use(express.static(__dirname));
 
-
 app.get('/Page', (req, res) => {
-    res.sendFile(__dirname + "/Page.html");
+    res.sendFile(__dirname + "/TPA/Page.html");
 });
 
 app.get('/Page/:color', (req, res) => {
-    res.sendFile(__dirname + "/Page.html");
+    res.sendFile(__dirname + "/TPA/Page.html");
 });
 
 app.get('/provider*', (req, res) => {
-    res.sendFile(__dirname + "/Page.html");
+    res.sendFile(__dirname + "/TPA/Page.html");
 });
 
 app.get('/Widget', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/TPA/index.html");
 });
 
 app.get('/menu', (req, res) => {
-    res.sendFile(__dirname + "/Menu.html");
+    res.sendFile(__dirname + "/TPA/Menu.html");
 });
 
 app.get('/client*', (req, res) => {
-    res.sendFile(__dirname + "/client.html");
+    res.sendFile(__dirname + "/TPA/client.html");
 });
-
 
 app.get('/gluedWidget*', (req, res) => {
-    res.sendFile(__dirname + "/Fixed.html");
+    res.sendFile(__dirname + "/TPA/Fixed.html");
 });
 
+https.createServer(options, app).listen(5000, () => console.log("Server is running on port 5000"));
 
-
-
-
-app.listen(5000, () => console.log('Example app listening on port 3000!'))
