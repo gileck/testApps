24e0f5b9b4ca93743c2ceb587945143b7c605736
Gil Eckstein <gileck@wix.com>
2018-06-20 17:18:53 +0300

    commit

--- WixCodeInstallTestApp/editor.js
+++ WixCodeInstallTestApp/editor.js
@@ -1,8 +1,9 @@
 module.exports = {
     editorReady: function (editorSDK) {
         const WIX_CODE_APP_DEF_ID = '675bbcef-18d8-41f5-800e-131ec9e08762'
-        editorSDK.document.application.install(WIX_CODE_APP_DEF_ID);
+        editorSDK.document.application.install('token', {appDefinitionId: WIX_CODE_APP_DEF_ID});
     },
     onEvent: () => {},
     getAppManifest: () => {}
 };
+
